c1d5d818956a763a0e41bad952fd36b9a5ff15e6
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2022-05-04 21:40:04 +0700

    user service

---
+++ my-client/src/app/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError, catchError, retry, Observable } from 'rxjs';
+const baseUrl: string = "http://localhost:3000"
+const API_URL = 'http://localhost:3000/api/test/';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  // getAllUsers(): Observable<IDuser[]>{
+  //   return this._http.get<IDuser[]>(`${baseUrl}/users`)
+  //   .pipe(
+  //     retry(3),
+  //     catchError(this.handleError)
+  //   )
+  // }
+
+  // handleError(error: HttpErrorResponse){
+  //   return throwError(()=> new Error(error.message))
+  // }
+
+
+  getAllUsers(): Observable<IDuser[]> {
+    return this.http.get<IDuser[]>(`${baseUrl}/users`)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      )
+  }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError(() => new Error(error.message))
+  }
+}
