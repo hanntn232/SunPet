c50d190f1533ae16a925d02b06e8c303b50d53dc
lethikimquy0808 <quyltk19406@st.uel.edu.vn>
2022-05-10 23:07:26 +0700

    Tasklist
    - Sửa lại css và html Tasklist của dasboarAdmin
    - Chức năng thêm việc cần làm

--- my-client/src/app/admintasklist/admintasklist.component.ts
+++ my-client/src/app/admintasklist/admintasklist.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { Todo } from '../model/todo';
+import { ToastrService } from 'ngx-toastr';
+import { TasklistService } from '../service/tasklist.service';
 
 @Component({
   selector: 'app-admintasklist',
@@ -7,9 +17,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdmintasklistComponent implements OnInit {
 
-  constructor() { }
+  todos: any;
+  todo: Todo = new Todo();
+  errorMessage: string = '';
+
+  constructor(private _service: TasklistService, private _toast: ToastrService) { }
 
   ngOnInit(): void {
+    this.getAllTodoItems();
+  }
+
+  getAllTodoItems() {
+    this._service.getTodoList().subscribe({
+      next: (data) => (this.todos = data),
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
+  submitData(form: NgForm) {
+    // console.log("Form data: ",  form.value);
+    console.log('Model: ', this.todo);
+
+    // Insert todoitems
+    if (this.todo.id == '') {
+      this._service.postTodo(this.todo).subscribe((res) => {
+        let resData = JSON.parse(JSON.stringify(res)); 
+        if (resData.message === 'Success') {
+          // alert('Success!');
+          this._toast.success("Inserted successfully!", "Success!")
+          this.getAllTodoItems();
+        } else {
+          alert('Fail!');
+        }
+      });
+    }
+    //  Update todo items
+    else {
+      this._service
+        .updateTodo(this.todo.id, this.todo)
+        .subscribe((res) => {
+          let resData = JSON.parse(JSON.stringify(res));
+          if (resData.message === 'Success') {
+            // alert('Update Successfully!');
+            this._toast.success("Updated successfully!", "Success!")
+            this.onReset(form);
+            this.getAllTodoItems();
+          } else {
+            alert(resData.message);
+          }
+        });
+    }
+  }
+
+  onEdit(data: Todo) {
+    this.todo = data;
+  }
+
+  onReset(form?: NgForm) {
+    if (form) {
+      form.reset();
+    }
+    this.todo = new Todo();
+  }
+
+  onDelete(id: any, form: NgForm) {
+    if (confirm('Are you sure you want to delete this product?') == true) {
+      this._service.deleteTodo(id).subscribe((res) => {
+        let resData = JSON.parse(JSON.stringify(res));
+        if (resData.message === 'Success') {
+          // alert('Delete Successfully!');
+          this._toast.info("Deleted successfully!", "Success!", {
+            timeOut: 2000
+          })
+          this.onReset(form);
+          this.getAllTodoItems();
+        } else {
+          alert(resData.message);
+        }
+      });
+      this.onReset(form);
+    }
   }
 
 }
---
+++ my-client/src/app/service/tasklist.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable, retry, throwError, catchError } from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http'
+import { Todo } from '../model/todo';
+const baseUrlTodo: string="http://localhost:3000"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasklistService {
+  public selectedId: any;
+  constructor(private _http: HttpClient) {
+   }
+   ngOnInit(): void {
+    this.getTodoList();
+  }
+
+  getTodoList(): Observable<Todo[]>{
+    return this._http.get<Todo[]>(`${baseUrlTodo}/todos`)
+    .pipe(
+      retry(3),
+      catchError(this.handleError)
+    )
+  }
+  postTodo(data:Todo){
+    return this._http.post<Todo[]>(`${baseUrlTodo}/todos`, data);
+  }
+  updateTodo(id:string, newdata:Todo):Observable<any>{
+    return this._http.patch(`${baseUrlTodo}/todos`, newdata);
+  }
+  deleteTodo(id:string){
+    return this._http.delete(`${baseUrlTodo}/${id}`);
+  }
+  handleError(error: HttpErrorResponse){
+    return throwError(()=> new Error(error.message))
+  }
+}
