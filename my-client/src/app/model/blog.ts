f0b38b2d861d0daa8712d03d3c393ea4fd991e1c
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2022-05-10 10:53:08 +0700

    revert lỗi lần 1

--- my-client/src/app/model/blog.ts
+++ my-client/src/app/model/blog.ts
@@ -1,15 +1,15 @@
-export class IDblog{
-    id: string;
-    date: string;
-    title: string;
-    content: string;
-    image: string;
-    constructor (){
-        this.id = '';
-        this.date='';
-        this.title='';
-        this.content='';
-        this.image='';
+export interface IDblog{
+    id: string,
+    date: string,
+    title: string,
+    content: {
+        part1: string,
+        part2: string,
+        part3: string
+    },
+    image: {
+        img1: string,
+        img2: string,
+        img3: string
     }
 }
-
