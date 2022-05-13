606f324e6cdc257b19dc4bba017af97bb8abb30f
CamTu23 <tuntc19406@st.uel.edu.vn>
2022-05-13 17:22:27 +0700

    commit product-admin get,change,delete product

--- my-client/src/app/service/product-detail.service.ts
+++ my-client/src/app/service/product-detail.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable, retry, throwError, catchError } from 'rxjs';
 import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http'
 import { IDproduct } from '../model/product';
-import { Product } from '../model2/product';
 const baseUrlProduct: string="http://localhost:3000"
 @Injectable({
   providedIn: 'root'
@@ -13,33 +12,26 @@ export class ProductDetailService {
   constructor(private _http: HttpClient){ 
   }
   ngOnInit(): void {
-    
+    this.getProductList();
   }
 
-  getProductList(): Observable<Product[]>{
-    return this._http.get<Product[]>(`${baseUrlProduct}/products`)
+  getProductList(): Observable<IDproduct[]>{
+    return this._http.get<IDproduct[]>(`${baseUrlProduct}/products`)
     .pipe(
       retry(3),
       catchError(this.handleError)
     )
   }
+  postProduct(data:IDproduct){
+    return this._http.post<IDproduct[]>(`${baseUrlProduct}/products`, data);
+  }
+  updateProduct(id:string, newdata:IDproduct):Observable<any>{
+    return this._http.patch(`${baseUrlProduct}/products`, newdata);
+  }
+  deleteProduct(id:string){
+    return this._http.delete(`${baseUrlProduct}/${id}`);
+  }
   handleError(error: HttpErrorResponse){
     return throwError(()=> new Error(error.message))
   }
-
-  // getProductInfo(productID: string){
-  //   this.getProductList().subscribe({
-  //     next: productList => {
-  //       for(var i=0; i<productList.length; i++){
-  //         if(productList[i]._id == productID){
-  //           return productList[i];
-  //         }
-  //       }
-  //       return "Product not found"
-  //     },
-  //     error: err => {
-  //       return err.message
-  //     }
-  //   })
-  // }
 }
