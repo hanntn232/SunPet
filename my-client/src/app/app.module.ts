b86a32d996668b2f49f875011fefc6ee0f1ed3b3
lethikimquy0808 <quyltk19406@st.uel.edu.vn>
2022-05-17 17:48:10 +0700

    phan trang product
    phan trang product

--- my-client/src/app/app.module.ts
+++ my-client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -105,7 +105,9 @@ import { NgxPaginationModule } from 'ngx-pagination';
     }),
     NgxPaginationModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
-})
+}
+)
 export class AppModule { }
